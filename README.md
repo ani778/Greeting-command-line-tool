cbeff792557a6822cad192ea8a83fbca378fe2d5
ahovhann_elcomp <ahovhann@estee.com>
2024-07-23 13:11:13 +0400

    Greeting CLI Tool

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Greeting-command-line-tool
+
+This is a command-line interface (CLI) tool built with Node.js that greets users with a custom message. It allows you to specify a name, a verbosity level, and a custom greeting message.
+
+1. Initialize a new Node.js project by running the command npm init.
+2. Created an index.js file
+3. Added bin to your package.json
+4. I chose Commander for parsing command-line arguments effortlessly.
+5. To add some color I install Chalk.
+
+## Features
+
+- Accepts a `--name` or `-n` argument to specify the name.
+- Accepts a `--level` or `-l` (or `-lvl`) argument to specify the verbosity level (1 or 2).
+- Accepts an optional `--greeting` or `-g` argument for a custom greeting message.
+
+## Installation
+
+To use this tool, you need to have [Node.js](https://nodejs.org/) installed. Then, you can clone this repository and install the dependencies:
+
+```sh
+git clone <repository-url>
+cd Greeting-command-line-tool
+npm install
---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+// const { program } = require('commander');
+import { program } from 'commander';
+import chalk from 'chalk';
+
+program
+    .version("1.0.0")
+    .description("Greeting CLI")
+    .option('-n, --name <name>', 'enter Name', 'Guest')
+    .option('-l, --level <type>', 'verbosity level (1 or 2)', '1')
+    .option('-lvl, --lvl <type>', 'verbosity level (1 or 2)', '1')
+    .option('-g, --greeting <type>', 'custom greeting message', 'Hello')
+    .parse(process.argv)
+
+// console.log(program.opts())
+const options = program.opts();
+const name = options.name ;
+const level =parseInt(options.level || options.lvl, 10);
+const greeting = options.greeting;
+
+const getCurrentDateTime = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const date = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+};
+
+if (level === 1) {
+    console.log(chalk.blue(`${greeting}, ${name}!`));
+} else if (level === 2) {
+    const currentDateTime = getCurrentDateTime();
+    console.log(chalk.green(`${greeting}, ${name}! (Date and Time: ${currentDateTime})`));
+} else {
+    console.log(chalk.red('Invalid verbosity level. Please specify 1 or 2.'));
+}
+
